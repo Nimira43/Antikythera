44fcb39bc53cc478eadb32984a13850f708801d6
Nimira43 <n.rabone72@aol.com>
2026-07-06 18:24:42 +0100

    Bun and Next JS setup.

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
+export const metadata: Metadata = {
+  title: 'Antikythera',
+  description: 'Monorepo application using Next JS, Nest JS and Bun.',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang='en'
+      className={poppins.className}
+    >
+      <body className='min-h-full flex flex-col'>{children}</body>
+    </html>
+  )
+}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='p-10'>
+      <h1 className='text-main text-3xl'>
+        Antikytherea
+      </h1>
+    </div>
+  )
+}
